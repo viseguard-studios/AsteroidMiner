c347f595860e783914f0ab0898faca0cd6624288
Sike Ádám <molteneq.official@gmail.com>
2021-04-06 22:47:38 +0200

    Added string manipulators

--- src/com/viseguardstudios/asteroid_miner/map_loader/FileOpener.java
+++ src/com/viseguardstudios/asteroid_miner/map_loader/FileOpener.java
@@ -31,6 +31,50 @@ public class FileOpener {
         return commands;
     }
 
+    /**
+     * Cuts off parts commented with '#'
+     * @param input
+     * @return
+     */
+    public  String cutComments(String input){
+        return input.split("#")[0];
+
+    }
+
+    public ArrayList<String> getNameAndPropsFromLine(String input) throws Exception {
+
+        ArrayList<String> found = new ArrayList<>();
+
+        try {
+            String temp = input.split("\\{")[1];
+            temp = temp.split("\\}")[0];
+            String[] foundParams = temp.strip().split(" ");
+            for (int i = 0; i < foundParams.length; i++){
+                if (!foundParams[i].isEmpty()) {
+                    found.add(foundParams[i]);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Bad format in object describer");
+        }
+
+        return found;
+    }
+
+    public String[] getPropNameAndValue(String input) throws Exception {
+        String[] temp = input.strip().split("=");
+        try {
+            temp[1] = temp[1].split("\"")[1];
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Bad format in object property");
+        }
+        return temp;
+    }
+
     @Override
     protected void finalize() throws Throwable {
         input.close();
