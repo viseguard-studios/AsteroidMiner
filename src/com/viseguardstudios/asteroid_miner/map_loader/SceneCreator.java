4d8cea4e1b034a51653287992cfec1a0ea80f90e
Sike Ádám <molteneq.official@gmail.com>
2021-04-07 14:12:33 +0200

    Made some changes to map loader

--- src/com/viseguardstudios/asteroid_miner/map_loader/SceneCreator.java
+++ src/com/viseguardstudios/asteroid_miner/map_loader/SceneCreator.java
@@ -1,4 +1,18 @@
 package com.viseguardstudios.asteroid_miner.map_loader;
 
+import com.viseguardstudios.asteroid_miner.model.Scene;
+
+import java.util.ArrayList;
+
 public class SceneCreator {
+    /** TODO This is incomplete!
+     * Create a new Scene by passing the command lines
+     * @return
+     */
+    public static Scene SceneFromCommandLine(ArrayList<String> inputLines){
+        Scene scene = new Scene();
+
+
+        return scene;
+    }
 }
---
+++ src/com/viseguardstudios/asteroid_miner/map_loader/resource/UraniumCreator.java
@@ -0,0 +1,13 @@
+package com.viseguardstudios.asteroid_miner.map_loader.resource;
+
+import com.viseguardstudios.asteroid_miner.map_loader.ResourceCreator;
+import com.viseguardstudios.asteroid_miner.model.item.Uranium;
+
+public class UraniumCreator extends ResourceCreator {
+
+    public static Uranium UraniumFromCommandLine(String input){
+
+        
+
+    }
+}
