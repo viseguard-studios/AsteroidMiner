4672a4b97cd2e097d1eb516903be29835e329c03
Titusz <tataititi2@gmail.com>
2021-04-19 11:52:26 +0200

    Updated UFO

--- src/com/viseguardstudios/asteroid_miner/model/entities/Vessel/UFO.java
+++ src/com/viseguardstudios/asteroid_miner/model/entities/Vessel/UFO.java
@@ -1,5 +1,6 @@
 package com.viseguardstudios.asteroid_miner.model.entities.Vessel;
 
+import com.viseguardstudios.asteroid_miner.model.Player;
 import com.viseguardstudios.asteroid_miner.model.entities.Asteroid;
 import com.viseguardstudios.asteroid_miner.model.inventory.IInventory;
 import com.viseguardstudios.asteroid_miner.model.inventory.InfiniteInventory;
@@ -18,19 +19,34 @@ public class UFO extends Vessel{
 
     @Override
     public AsteroidPlaces getPlace() {
-        return null;
+        return AsteroidPlaces.Vessel;
     }
 
     @Override
     public int GetHidingSpaceRequirement() {
-        return 1;
-    }
+        return 2;
+    } 
 
 
     //Nem képes bányászni
     @Override
     public void Drill() {
         //Do nothing
+    }
+
+    @Override
+    public void setOwner(Player p) {
+        //Do nothing
+    }
+
+    @Override
+    public void SolarFlare() {
+        //DO not die
+    }
+
+    @Override
+    public void Hide() {
+        //DO not hide
     }
 
     @Override
