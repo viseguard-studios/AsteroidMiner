477bbdb06ac0b0d78f4f52f2e88116fbb7047c73
dpeter99 <dpeter99@gmail.com>
2021-04-25 20:45:47 +0200

    Add player list

---
+++ src/com/viseguardstudios/asteroid_miner/view/panels/ingame/PlayerList.java
@@ -0,0 +1,19 @@
+package com.viseguardstudios.asteroid_miner.view.panels.ingame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerList extends JPanel {
+
+    public PlayerList() {
+
+        FlowLayout layout = new FlowLayout(FlowLayout.RIGHT);
+        this.setLayout(layout);
+
+        JLabel l = new JLabel();
+        l.setText("asd");
+
+        this.add(l);
+
+    }
+}
